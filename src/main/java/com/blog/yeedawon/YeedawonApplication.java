d2d984d6938a1acd7b8571759a1f83dc7c3060a4
yeedawon <dlekdnjs34@gmail.com>
2026-02-26 10:55:28 +0900

    first setting

---
+++ src/main/java/com/blog/yeedawon/YeedawonApplication.java
@@ -0,0 +1,13 @@
+package com.blog.yeedawon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeedawonApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(YeedawonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/yeedawon/YeedawonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.yeedawon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeedawonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
