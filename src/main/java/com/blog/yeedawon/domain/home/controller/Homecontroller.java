c6c3113198f4ae56e04f1951a75ad31e293b2d80
yeedawon <dlekdnjs34@gmail.com>
2026-02-26 19:09:40 +0900

    path variable

--- src/main/java/com/blog/yeedawon/domain/home/controller/Homecontroller.java
+++ src/main/java/com/blog/yeedawon/domain/home/controller/Homecontroller.java
@@ -1,8 +1,7 @@
 package com.blog.yeedawon.domain.home.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller //스프링 부트에게 이 클래스는 Controller의 역할을 명시하는 annotation
 public class Homecontroller {
@@ -29,7 +28,17 @@ public class Homecontroller {
     @ResponseBody
     // 스프링 부트는 쿼리 스트링의 parameter를 method의 parameter로 받을 수 있다.
     // 규칙! : 쿼리 스트링의 parameter 변수명과 method의 parameter 변수 명을 동일하게 해야함
-    public int showPlus(int a, int b) {
+    // 각 변수 앞에 @RequestParam("default value")을 설정할 경우 해당 변수에 값이 대입되어있지 않을 때 default value로 초기화되어 메소드 실행
+    // 주의! : @RequestParam(String), 기본값은 String이고, int 값이 아님
+    public int showPlus(int a, @RequestParam("0") int b) {
+        return a + b;
+    }
+
+    //path variable 방식
+    @GetMapping("/home/plus2/{a}/{b}")
+    @ResponseBody
+    //각 변수 앞에 PathVariable annotation 추가해야함
+    public int showPlus2(@PathVariable int a,@PathVariable int b) {
         return a + b;
     }
 }
